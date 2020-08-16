85263777100e93e9f6c9cda1c9b5be4997e602e0
wise-king-sullyman <asullivan96@live.com>
2020-08-16 13:12:24 -0400

    Completed logic

---
+++ sub-string.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def substrings(string,dictionary)
+  string_lower = string.downcase
+  found_words = dictionary.select{|word| string_lower.include? word}
+  found_words.reduce(Hash.new(0)) do |hash, word|
+    hash[word] = string_lower.scan(/#{word}/).length
+    hash
+  end
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low",
+  "own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
